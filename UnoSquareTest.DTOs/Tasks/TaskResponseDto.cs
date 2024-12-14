95cf1a3969d3379ffe2161beed5c0042dc494e59
SanderGCC <sandergcc@gmail.com>
2024-12-13 19:49:08 -0500

    Dto

--- UnoSquareTest.DTOs/Tasks/TaskResponseDto.cs
+++ UnoSquareTest.DTOs/Tasks/TaskResponseDto.cs
@@ -6,6 +6,6 @@
         public string? Name { get; set; }
         public string? Description { get; set; }
         public DateTime? DueDate { get; set; }
-        public bool Complete { get; set; }
+        public bool? Complete { get; set; }
     }
 }
